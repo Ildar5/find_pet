d1295f4864573b22e8af59d4dbeedbb0db0b7c8e
ildar5 <ildar.ildorado@gmail.com>
2020-05-07 01:21:23 +0300

    auth,login/signup added

--- pitomes/settings.py
+++ pitomes/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'pitomes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/pets'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
--- pitomes/urls.py
+++ pitomes/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('pets/', include('pets.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
